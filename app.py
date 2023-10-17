4b832b4713e5c2a61bf02eeb547db2c0334dae15
Bryan Rambo <btrambo@buffalo.edu>
2023-10-17 16:44:23 -0400

    mongodb stuff

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask
+import json
+import socketserver
+import sys
+from util.request import Request
+from pymongo import MongoClient
+import bcrypt
+import secrets
+from hashlib import sha256
 
 app = Flask(__name__)
 
+mongo_client = MongoClient("mongo")
+db = mongo_client["cse-312-hw"]
+chat_collection = db["chat-messages"]
+user_info = db["user-credentials"]
+token_storage = db["authentication-tokens"]
 
 @app.route('/')
 def hello_world():  # put application's code here
