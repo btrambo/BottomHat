926b7c8a61fb6edf8a56e30b87eb898fd5518c02
dwu35 <dwu35@buffalo.edu>
2023-11-01 19:18:45 -0400

    added database conversion in properties.py

--- properties.py
+++ properties.py
@@ -1,3 +1,4 @@
+from app import quiz_collection 
 
 class quizInput:
     def __init__(self, title, username, options, correct):
@@ -8,5 +9,20 @@ class quizInput:
 
 def convert_mongo_to_quizInput():
     # convert database of quiz questions into list of quizInputs format above
-    print("placeholderf for error get rid of when starting function")
+    
+    arr = []
+    # .find the documents in collection and store everything first
+    # 
+    data = quiz_collection.find()
+    for document in data:
+        # for key,value in document.items():
+        usernames = document['username']
+        titles = document['title']
+        questions = document['options'] #options is going to have 3 values, should be able to set as a key:value with value as an arr
+        answers = document['answers'] #going to have 1 2 or 3
 
+        document = quizInput(usernames,titles,questions,answers)
+
+        arr.append(document)
+
+    return arr
