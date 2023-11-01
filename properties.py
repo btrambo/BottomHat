31bebb3dd755a126e8c23df1f947e0b15a8b4804
Bryan Rambo <btrambo@buffalo.edu>
2023-11-01 13:45:13 -0400

    quizInput class, conversion defined

---
+++ properties.py
@@ -0,0 +1,12 @@
+
+class quizInput:
+    def __init__(self, title, username, options, correct):
+        self.title = title
+        self.username = username
+        self.options = options
+        self.correct_response = correct # string either option 1, option2, or option 3
+
+def convert_mongo_to_quizInput():
+    # convert database of quiz questions into list of quizInputs format above
+    print("placeholderf for error get rid of when starting function")
+
